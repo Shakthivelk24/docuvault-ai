372d43b99779f2e2fecea60e67ab66173c80368f
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-08-24 19:27:32 +0530

    feat(client): integrate ClerkProvider and BrowserRouter in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+createRoot(document.getElementById("root")).render(
+  <ClerkProvider publishableKey={clerkPubKey}>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </ClerkProvider>,
+);
