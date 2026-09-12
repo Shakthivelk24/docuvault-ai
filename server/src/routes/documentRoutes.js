e14bf19947fada7590d2d2874d16478d070db04e
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-09-12 18:08:58 +0530

    feat(api): add endpoint for upload completion handling

--- server/src/routes/documentRoutes.js
+++ server/src/routes/documentRoutes.js
@@ -4,11 +4,13 @@ import {
 	getDocument,
 	getDocuments,
 	getPreviewUrl,
+	completeUpload,
 } from "../controllers/documentController.js";
 
 const router = express.Router();
 
 router.post("/upload-url", createUploadUrl);
+router.post("/:id/upload-complete", completeUpload);
 router.get("/", getDocuments);
 router.get("/:id/preview", getPreviewUrl);
 router.get("/:id", getDocument);
